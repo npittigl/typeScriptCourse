5b0daeab07243eaeec840c0f245af3bd51b8994d
nadiapittiglio <npittigl@gmail.com>
2024-05-13 13:28:39 -0400

    installing webpack

---
+++ 11_Webpack/07_AddingProductionWorkflow/webpack-04-added-prod-workflow/webpack.config.prod.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new CleanPlugin.CleanWebpackPlugin()
+  ]
+};
